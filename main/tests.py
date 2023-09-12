c9772034e7d984d0c8aae8a25aff5da6052bf395
shabhias <shabhialiyya3101@gmail.com>
2023-09-12 23:51:26 +0700

    membuat model sesuai soal

--- main/tests.py
+++ main/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
 class mainTest(TestCase):
--- main/views.py
+++ main/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def show_main(request):
+    
+    product_deskmate = Product(
+        name = 'Deskmate',
+        amount = 10,
+        price = 150000,
+        description = 'dengan DESKMATE yang sesuai dengan seleramu, membuat kamu nyaman saat nugas di atas meja'
+    )
+   
+    product_lightbar = Product(
+        name = 'Monitor Lightbar',
+        amount = 20,
+        price = 200000,
+        description = 'dengan MONITOR LIGHTBAR memberikan estetika pada set up komputer atau laptop di meja Anda'
+    )
+
+    
     context = {
-        'name': 'Shabhi',
-        'class': 'PBP D'
+        'product_deskmate': product_deskmate,
+        'product_lightbar': product_lightbar,
+        'product1': 'Decor Mini Painting',
+        'amount1': '13',
+        'product2': 'Miniatur Figure',
+        'amount2': '10',
     }
 
     return render(request, "main.html", context)
