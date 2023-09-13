0d7aeda81c152f67a8df0ba04f82842d17f8ef9d
shabhias <shabhialiyya3101@gmail.com>
2023-09-13 10:15:26 +0700

    menambahkan test case

--- main/tests.py
+++ main/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.test import TestCase, Client
+from .models import Product
 
 # Create your tests here.
 class mainTest(TestCase):
@@ -10,3 +11,28 @@ class mainTest(TestCase):
     def test_main_using_main_template(self):
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
+    
+    def setUp(self):
+        # Membuat objek Product Deskmate
+        self.deskmate = Product.objects.create(
+            name='Deskmate',
+            amount=10,
+            price=150000,
+            description='dengan DESKMATE yang sesuai dengan seleramu, membuat kamu nyaman saat nugas di atas meja'
+        )
+
+        # Membuat objek Product Monitor Lightbar
+        self.lightbar = Product.objects.create(
+            name='Monitor Lightbar',
+            amount=20,
+            price=200000,
+            description='dengan MONITOR LIGHTBAR memberikan estetika pada set up komputer atau laptop di meja Anda'
+        )
+
+    def test_deskmate_amount(self):
+        self.assertEqual(self.deskmate.amount, 10)
+
+    def test_lightbar_amount(self):
+        self.assertEqual(self.lightbar.amount, 20)
+        
+    
