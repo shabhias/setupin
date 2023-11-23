dc544ca48377447e415eb26d334a280ea134c2ed
shabhias <shabhialiyya3101@gmail.com>
2023-11-23 09:55:52 +0700

    deploy terus

--- setupin/settings.py
+++ setupin/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'setupin.urls'
